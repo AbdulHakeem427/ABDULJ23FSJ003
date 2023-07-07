148bee57e97468d0146157dd5672e84e4bfa73ec
Abdul Hakeem <98639825+AbdulHakeem427@users.noreply.github.com>
2023-07-07 19:04:47 +0530

    Update Calculator.java

--- assignments_1/Calculator.java
+++ assignments_1/Calculator.java
@@ -3,83 +3,81 @@ package assignments_1;
 import java.util.Scanner;
 	
 class   Calculator{
-	public static void main(String[] args) {
-		Scanner scan=new Scanner(System.in);
-		System.out.println("Please Enter First Number...: ");
-		String num1=scan.next();
-		
-		System.out.println("Please Enter Second Number...: ");
-		String num2=scan.next();
-		
+//	public static void main(String[] args) {
+//		Scanner scan=new Scanner(System.in);
+//		System.out.println("Please Enter First Number...: ");
+//		String num1=scan.next();
+//		
+//		System.out.println("Please Enter Second Number...: ");
+//		String num2=scan.next();
+//		
+//	
+//		Calculator casio=new Calculator();
+//		casio.add(num1, num2);
+//		casio.sub(num1, num2);
+//		casio.mul(num1, num2);
+//	//	casio.div(num1, num2);
+//		
+//		
+//		scan.close();
+//		
+//	}
 	
-		Calculator casio=new Calculator();
-		casio.add(num1, num2);
-		casio.sub(num1, num2);
-		casio.mul(num1, num2);
-		casio.div(num1, num2);
-		//casio.modulus(num1, num2);
-		
-		scan.close();
-		
-	}
-	
-	public String add(String i,String j) {
+	public Integer add(String i,String j) {
 		int result=Integer.parseInt(i)+Integer.parseInt(j);
 		System.out.println("The Additon of "+i+"and"+j+" is..:"+result);
 		
-		return  "result";
+		return  result;
 	}
 	
-	public String sub(String i,String j) {
+	public int sub(String i,String j) {
 		int result=Integer.parseInt(i)-Integer.parseInt(j);
 		System.out.println("The Subtraction of "+i+"and"+j+"  is..:"+result);
 		
-		return  "result";
+		return  result;
 	}
 	
-	public String mul(String i,String j) {
+	public int mul(String i,String j) {
 		int result=Integer.parseInt(i)*Integer.parseInt(j);
 		System.out.println("The Multipication of "+i+"and"+j+"  is..:"+result);
 		
-		return  "result";
+		return  result;
 	}
 	
-//	public String division(String i,String j) {
-//		int result;
-//		try {
-//		result=Integer.parseInt(i)/Integer.parseInt(j);
-//		System.out.println("The Division of "+i+"and"+j+"  is..:"+result);
-//		}catch(ArithmeticException e3) {
-//			System.out.println("Please Enter a value other than Zero..: ");
-//		}catch(NumberFormatException ee4) {
-//			System.out.println("Please enter a valid number, not alphabets");
-//		}
-//		return  "result";
-//	}
-	
-	public String div(String i,String j) {
-		int result;
-		int n=Integer.parseInt(j);
-		if(n>0) {
-			 result=Integer.parseInt(j)/Integer.parseInt(j);
-		}else {
-			System.out.println("Please enter a value other thean zero");
+	public double div(int i,int j) {
+		double result = 0;
+		 if (j != 0) {
+	            result = i / j;
+	           // return result;
+	        } else {
+	            System.out.println(" Please enter a valid number, division by zero is not allowed..");
+	        }
+		return result;
+		
 		}
-		
-		
-		
-		return "result";
+
+	    public  double calculatePower(double base, int exponent) {
+	        double result = 1;
+
+	        if (exponent >= 0) {
+	            for (int i = 1; i <= exponent; i++) {
+	                result *= base;
+	            }
+	        } else {
+	            for (int i = -1; i >= exponent; i--) {
+	                result /= base;
+	            }
+	        }
+
+	        return result;
+	    }
 	}
-	
-
-}
-	
-	
 
 
 	
+
 	
-}
 	
-	
+
+
 
