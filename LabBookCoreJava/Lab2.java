b4506cfbf3a373226e71d004702e4ee81bb8a177
Abdul Hakeem <98639825+AbdulHakeem427@users.noreply.github.com>
2023-07-24 08:20:42 +0530

    Update Lab2.java

--- LabBookCoreJava/Lab2.java
+++ LabBookCoreJava/Lab2.java
@@ -2,7 +2,9 @@ package LabBookCoreJava;
 
 public class Lab2 {
 	    public static void main(String[] args) {
-	    	int i;
+	   	String str=args[0];
+	    	System.out.println("Command Ling Argument is: "+str);
+	    	
 
 	    }
 }	   
